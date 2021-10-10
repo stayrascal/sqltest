389c05af7e10e8ac2d68959f98ae34a8768b4145
stayrascal <zpwu@thoughtwroks.com>
2021-10-11 00:02:48 +0800

    [xxx][stayrascal] add setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setup(name="sqltest",
+          version="0.0.1",
+          author="stayrascal",
+          author_email="stayrascal@gmail.com",
+          description="sqltest: easy testing ETL SQLs",
+          long_description=long_description,
+          keywords=["test", "ETL", "sql"])
