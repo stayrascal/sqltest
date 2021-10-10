70339ec393ba5fb55080dbef864e5ca51a0ccef1
stayrascal <zpwu@thoughtwroks.com>
2021-10-10 22:16:46 +0800

    [xxx][stayrascal] use project path replace local path

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 from pyspark.sql import SparkSession
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 SPARK = (SparkSession
          .builder
--- tests/integrations/spark/test_csv_dataset.py
+++ tests/integrations/spark/test_csv_dataset.py
@@ -2,17 +2,17 @@ from unittest import TestCase
 
 from sql.datasource import CsvDatasetReader
 from sql.runner.spark import SparkRunner
-from tests import SPARK
+from tests import PROJECT_PATH, SPARK
 
 
 class TestCsvDataSource(TestCase):
     def test_csv_data_source_demo(self):
         environments = {
             'env': 'dev',
-            'target_data_path': '/Users/wuzhiping/workspace/tools/sql-tester/tests/data/tables'
+            'target_data_path': f'{PROJECT_PATH}/tests/data/tables'
         }
         reader = CsvDatasetReader(
-            data_path='/Users/wuzhiping/workspace/tools/sql-tester/tests/data/cases/spark_etl_sql_test_demo')
-        sql_file_path = '/Users/wuzhiping/workspace/tools/sql-tester/tests/data/cases/spark_etl_sql_test_demo/spark_etl_demo.sql'
+            data_path=f'{PROJECT_PATH}/tests/data/cases/spark_etl_sql_test_demo')
+        sql_file_path = f'{PROJECT_PATH}/tests/data/cases/spark_etl_sql_test_demo/spark_etl_demo.sql'
         runner = SparkRunner(SPARK, environments)
         runner.run(reader, sql_file_path)
